cb81fd5c29ffdfa964049444b14b3bdce1a21067
ojasthakurx <ojasthakurx@gmail.com>
2023-01-27 05:32:21 +0530

    Tours Cards Moved Into Components

--- 02-backroads-project/backroads-react/src/components/Tours.js
+++ 02-backroads-project/backroads-react/src/components/Tours.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Title from "./Title";
 import { tours } from "../data";
-import ToursCards from "../ToursCards";
+import ToursCards from "./ToursCards";
 const Tours = () => {
   return (
     <section className="section" id="tours">
