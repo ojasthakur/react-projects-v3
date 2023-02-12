b254fe1c58aaae0ef48a23e25d6f2e9f58477d06
ojasthakurx <ojasthakurx@gmail.com>
2023-02-13 04:17:31 +0530

    progress

--- 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,27 +1,53 @@
 import { useEffect, useState } from "react";
 
 const url = "https://api.github.com/users";
-
+const url2 = "https://ojas-ecommerce.onrender.com/api/v1/products";
+const url3 = "https://ojas-ecommerce.onrender.com/api/v1/products";
 const FetchData = () => {
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
-      const res = await fetch("https://api.github.com/users");
-      const parsedData = await res.json();
-      setData(parsedData);
+      try {
+        const res = await fetch(url3, {
+          method: "GET",
+          headers: {
+            accept: "application/json",
+          },
+        });
+        console.log(res);
+        if (!res.ok) {
+          setIsError(true);
+          setIsLoading(false);
+          return;
+        }
+        const parsedData = await res.json();
+        console.log(parsedData.products);
+        setData(parsedData.products);
+      } catch (error) {
+        console.log(error);
+        setIsError(true);
+      }
+      setIsLoading(false);
     };
     fetchData();
-    console.log(data);
   }, []);
-
+  if (isLoading) {
+    return <h1>Loading....</h1>;
+  }
+  if (isError) {
+    return <h1>Oops, There was an error</h1>;
+  }
   return (
-    <div>
+    <div className="flex-container">
       {data.map((element) => {
         return (
-          <div key={element.id}>
-            <img src={element.avatar_url} className="image" alt="" />
-            <h4>{element.login}</h4>
+          <div key={element._id} className="flex-item">
+            <img src={element.image} className="flex-image" alt="" />
+            <h4>{element.name}</h4>
+            <p>{element.description}</p>
           </div>
         );
       })}
